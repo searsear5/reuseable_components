75ac741f726e419aa69ed3b8e87be02dc7604c5f
searsear5 <supanutbbp@gmail.com>
2024-04-12 01:42:46 +0700

    add bulma css

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import Profilecard from './Profilecard'
 import imgAlexa from './images/alexa.png'
 import imgCortana from './images/cortana.png'
@@ -7,10 +8,42 @@ function App() {
 
     return (
         <div>
-            <h1>Personal Digital Assistants</h1>
-            <Profilecard title={"Alexa"} handle={"@alex99"} img={imgAlexa} />
-            <Profilecard title={"Cortana"} handle={"@cortana23"} img={imgCortana} />
-            <Profilecard title={"Siri"} handle={"@siri101"} img={imgSiri} />
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <Profilecard
+                                title={"Alexa"}
+                                handle={"@alex99"}
+                                img={imgAlexa}
+                                description={"Alexa was created by Amazon and helps you buy things."} />
+                        </div>
+                        <div className='column is-4'>
+                            <Profilecard
+                                title={"Cortana"}
+                                handle={"@cortana23"}
+                                img={imgCortana}
+                                description={"Cortana was made by Microsoft. who knows what it does"} />
+                        </div>
+                        <div className='column is-4'>
+                            <Profilecard
+                                title={"Siri"}
+                                handle={"@siri101"}
+                                img={imgSiri}
+                                description={"Siri was made by Apple and is being phased out."} />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+
+
         </div>
     )
 }
--- pdas/src/Profilecard.js
+++ pdas/src/Profilecard.js
@@ -1,10 +1,21 @@
 function Profilecard(props) {
-    const { title, handle, img } = props
+    const { title, handle, img, description } = props
     return (
-        <div>
-            <img src={img} alt="profile card image" />
-            <h2>Title is {title}</h2>
-            <h2>Handle is {handle}</h2>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={img} alt="profile card image" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-5">{handle}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
+
+
         </div>
     )
 }
